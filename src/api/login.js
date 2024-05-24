a6276fef3fb993b1a104f31ff76e4c31119c6475
JoritzAres <147603322+JoritzAres@users.noreply.github.com>
2024-05-24 09:41:56 +0200

    sisi confio

--- src/api/login.js
+++ src/api/login.js
@@ -1,7 +1,7 @@
 import { config } from '../config';
 
 // const base_url = `${config.server}/Login`;
-const base_url = `${config.server}:${config.server_port}/Login`;
+const base_url = `https://${config.server}:${config.server_port}/Login`;
 
 export async function LoginUsuario(datos) {
     const url = `${base_url}/Login`;
