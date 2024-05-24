d22d213b353019ac023bee02b70fd0081ceb79ed
JoritzAres <147603322+JoritzAres@users.noreply.github.com>
2024-05-24 09:30:55 +0200

    sisi

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const config = {
     // server_port: 3001,
-    // server: '192.168.170.214',
-    server:'https://servidor-two.vercel.app',
+    server: '127.0.0.1',
+    // server:'https://servidor-two.vercel.app',
     server_port: 3001
   }
   
